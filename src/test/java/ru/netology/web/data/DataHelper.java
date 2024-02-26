2201a71ac15ae0c4fc9cdbc735e3fb8c65b5ca09
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-02-26 19:01:43 +0400

    Added card numbers and changed to static method getMaskedCardNumber

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -16,14 +16,14 @@ public class DataHelper {
     }
 
     public static CardInfo getFirstCardInfo() {
-        return new CardInfo("", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+        return new CardInfo("4000 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
     }
 
     public static CardInfo getSecondCardInfo() {
-        return new CardInfo("", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+        return new CardInfo("4000 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
     }
 
-    public String getMaskedCardNumber(String cardNumber) {
+    public static String getMaskedCardNumber(String cardNumber) {
         return "**** **** ****" + cardNumber.substring(15);
     }
 
